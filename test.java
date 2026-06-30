7c6ee6f2a2d093735e29934f42133db5e9b6f864
kisko <kiskoton@gmail.com>
2026-06-30 23:35:56 +0200

    Move test.java into firstRepo folder

--- test.java
+++ test.java
@@ -1,11 +1,5 @@
 public class test {
     public static void main(String[] args) {
        System.out.println("HELLO!"); 
-       for(int i = 0; i <10; i++) {
-            if(i%2 == 0 && i != 0 ) continue;
-            System.out.print(" ".repeat(10 -i));
-            System.out.print("*".repeat(2*i + 1));
-            System.out.println();
-       }
     }
 }
