2a243382c7e934fd7c19ae8a8e8fcdfa41e86c13
kisko <kiskoton@gmail.com>
2026-07-01 20:56:18 +0200

    the creation of class point

---
+++ no_vibe/tocka.java
@@ -0,0 +1,23 @@
+
+public class tocka {
+
+    private double x;
+    private double y;
+
+    public tocka(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
